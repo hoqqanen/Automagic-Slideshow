e33bc977215aded3eaf9ba3f697b6beba435b73d
George Hokkanen <hokkanen@stanford.edu>
2012-03-28 10:58:38 -0700

    Slideshows now saved in pictures/

--- markov.py
+++ markov.py
@@ -7,8 +7,13 @@ import flickr
 import urllib
 import Image
 import time
+import datetime
 import sys
+import os
 from random import choice, shuffle
+
+time_now = datetime.datetime.now()
+folder_name = time_now.strftime('%d-%m,%H-%M-%S')
 
 tags = []
 alltags = set(tags)
@@ -17,13 +22,13 @@ def load_photo(url):
 	photo = Image.open(file)
 	return photo
 	
-def show_pic(tags):
+def show_pic(tags, idx):
 	photos = flickr.photos_search(tags=tags,per_page=50)
 	ppp = choice(photos)
 	u = ppp.getURL(size='Medium',urlType='source')
-	print u
 	ph = load_photo(u)
-	ph.show()
+	#ph.show()
+	ph.save("pictures/"+folder_name+"/"+str(idx)+".jpg","JPEG")
 	newtagset = tags[1:]
 	newtags = list(set(ppp.tags)-alltags)
 	if len(newtags)==0:
@@ -33,15 +38,18 @@ def show_pic(tags):
 
 
 def main(*tag_list):
-	print tag_list[1:]
+	os.mkdir('pictures/'+folder_name)
+	f = open('pictures/'+folder_name+'/log.txt','w')
 	tags = list(tag_list[1:])
+	f.write('0: '+str(tags)+'\n')
 	alltags = set(tags)
-	for i in range(10):
-		tags = show_pic(tags)
+	for i in range(20):
+		tags = show_pic(tags, i)
 		alltags = alltags | set(tags)
-		print tags
+		f.write(str(i+1)+': '+str(tags)+'\n')
 		#print alltags
-		time.sleep(.1)
+		#time.sleep(.1)
+	f.close()
 	
 if __name__ == '__main__':
 	sys.exit(main(*sys.argv))
